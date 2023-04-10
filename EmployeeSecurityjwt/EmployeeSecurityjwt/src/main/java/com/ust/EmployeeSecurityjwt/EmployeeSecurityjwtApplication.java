32c34f9d3ff4a869f54c4f1b8d240246dd2a2c3b
Devika Bijo <devikabijo03@gmail.com>
2023-04-10 17:20:05 +0530

    updates

---
+++ EmployeeSecurityjwt/EmployeeSecurityjwt/src/main/java/com/ust/EmployeeSecurityjwt/EmployeeSecurityjwtApplication.java
@@ -0,0 +1,13 @@
+package com.ust.EmployeeSecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSecurityjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSecurityjwtApplication.class, args);
+	}
+
+}
